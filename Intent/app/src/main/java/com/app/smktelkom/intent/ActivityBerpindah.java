3aa62e3371f95efe396e43fadd447dccb2a963be
kurniadi <kurniadiahmadwijaya@gmail.com>
2017-12-25 20:28:15 +0700

    Add 2nd Project : Intent , Finished : Default Intend

---
+++ Intent/app/src/main/java/com/app/smktelkom/intent/ActivityBerpindah.java
@@ -0,0 +1,13 @@
+package com.app.smktelkom.intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityBerpindah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_berpindah);
+    }
+}
