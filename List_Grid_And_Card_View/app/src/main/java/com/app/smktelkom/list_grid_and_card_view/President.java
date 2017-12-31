93f9b02c6bb34d7aadff4267f1d5856bbfe6d80a
kurniadi <kurniadiahmadwijaya@gmail.com>
2018-01-01 00:42:15 +0700

    Adding 4th Project : List View

---
+++ List_Grid_And_Card_View/app/src/main/java/com/app/smktelkom/list_grid_and_card_view/President.java
@@ -0,0 +1,36 @@
+package com.app.smktelkom.list_grid_and_card_view;
+
+/**
+ * Created by SMK TELKOM on 30/12/2017.
+ */
+
+public class President {
+
+        private String name, remarks, photo;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getRemarks() {
+            return remarks;
+        }
+
+        public void setRemarks(String remarks) {
+            this.remarks = remarks;
+        }
+
+        public String getPhoto() {
+            return photo;
+        }
+
+        public void setPhoto(String photo) {
+            this.photo = photo;
+        }
+
+
+}
